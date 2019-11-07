13cda1c27e64e2ac569c8348672ccbf3fdfa67d6
JK <jakub.krysakowski@gmail.com>
2019-11-07 18:32:15 +0100

    Remove verbose flag

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 _now=$(date +"%m_%d_%Y")
 _file="/home/users/johndoe/blog_backup/backups/blog-backup-$_now.tar.gz"
-tar -zcvf "$_file" /home/johndoe/irok84/public_html/blog
+tar -zcf "$_file" /home/johndoe/irok84/public_html/blog
 chmod 600 "$_file"
