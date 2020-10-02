efac44662b6b8c5fdc3aaf5179e0127928287e71
JakubTheDeveloper <jakub.krysakowski@gmail.com>
2020-10-02 08:09:27 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 _now=$(date +"%d_%m_%Y")
 _file="/home/users/johndoe/blog_backup/backups/blog-backup-$_now.tar.gz"
-tar -zcf "$_file" /home/johndoe/irok84/public_html/blog
+tar -zcf "$_file" /home/johndoe/public_html/blog
 chmod 600 "$_file"
